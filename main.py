723dbbdc413627412fa0d175626897cd0ca3ccd0
Saugat Poudel <saugatpoudel2054@gmail.com>
2025-08-10 15:12:39 +0545

    Basic prompting setup

--- main.py
+++ main.py
@@ -1,6 +1,28 @@
-def main():
-    print("Hello from agent!")
+import os
+import sys
 
+from dotenv import load_dotenv
+from google import genai
+from google.genai import types
 
-if __name__ == "__main__":
-    main()
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
+
+if len(sys.argv) < 2:
+    print("Please provide a prompt")
+    exit(1)
+
+prompt = sys.argv[1]
+
+messages = [
+    types.Content(
+        parts = [types.Part(text=prompt)],
+        role="user"
+    )
+]
+
+client = genai.Client(api_key=api_key)
+response = client.models.generate_content(model='gemini-2.0-flash-001', contents=messages)
+print(response.text)
+print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
